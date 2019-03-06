4677c1d057d3f38af6c246ca41c48b5d08621b7d
rramler <rudolf.ramler@scch.at>
2019-03-06 14:14:18 +0100

    Add structure and files for exercise 00

---
+++ 01-RingBufferTest/src/test/java/at/tuwien/swtesting/RingBufferTest.java
@@ -0,0 +1,14 @@
+package at.tuwien.swtesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RingBufferTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");  //TODO: Implement tests
+	}
+
+}
